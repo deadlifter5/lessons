ac599d13acc0ecd5d54173dcb1d664d6eef6bd46
deadlifter5 <162996190+deadlifter5@users.noreply.github.com>
2025-02-07 15:42:53 +0100

    Add files via upload

---
+++ homework_vue_5/task2/src/components/ItemsList/ItemComp.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div class="img-container">
+      <a target="_blank" :href="itemData.link">
+        <img :src="itemData.imgSrc" />
+      </a>
+      <span v-if="itemData.inStock">B наявності</span>
+      <h3>
+        <a target="_blank" :href="itemData.link"> {{ itemData.name }} </a>
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemComp",
+  props: {
+    itemData: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background: #000;
+  display: inline;
+  vertical-align: bottom;
+
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+}
+.img-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    width: 350px;
+    height: 300px;
+    border-radius: 10%;
+    padding: 12px;
+  }
+  a {
+    color: white;
+  }
+  span {
+    color: green;
+  }
+}
+</style>
---
+++ homework_vue_5/task3/src/constants/items_data.js
@@ -0,0 +1,26 @@
+export const itemsData = [
+  {
+    id: 1,
+    name: "Ноутбук ASUS TUF Gaming A15 FA506IV-HN198",
+    imgSrc: "https://content2.rozetka.com.ua/goods/images/big/25612928.jpg",
+    oldPrice: 59999,
+    newPrice: 51999,
+    link: "https://rozetka.com.ua/ua/asus_90nr03l1_m04340/p221358871/",
+  },
+  {
+    id: 2,
+    name: "Ноутбук ASUS TUF Gaming F15 FX506LH-HN153",
+    imgSrc: "https://content2.rozetka.com.ua/goods/images/big/238662391.jpg",
+    oldPrice: 38999,
+    newPrice: 32499,
+    link: "https://rozetka.com.ua/ua/asus-90nr03u1-m08940/p327560809/",
+  },
+  {
+    id: 3,
+    name: "Ноутбук HP Victus Gaming 16-s0014ua",
+    imgSrc: "https://content1.rozetka.com.ua/goods/images/big/375429329.jpg",
+    oldPrice: 39999,
+    newPrice: 34999,
+    link: "https://rozetka.com.ua/ua/hp-949x1ea/p401442651/",
+  },
+]
